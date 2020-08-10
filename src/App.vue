94d7e831d7032278e4c66ab2607ddac3c1b3852e
Adebayo Temitope <adecancode@gmail.com>
2020-08-10 22:26:47 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,9 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
-
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-
-body{
-  background: #f9f9f9;
-  font-family: 'Poppins', sans-serif;
 }
 </style>
