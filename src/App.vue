76f0b14e8b146904664a539e298c43b1e3147d58
Adebayo Temitope <adecancode@gmail.com>
2020-08-10 17:56:19 +0100

    Added Components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CountryDirectories />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountryDirectories from './components/CountryDirectories.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountryDirectories
   }
 }
 </script>
